70cf07ce488565eebe033dedbe3f46667f73e447
aditi-ashok <aditiash@usc.edu>
2023-04-15 12:52:22 -0700

    test
    change

--- pawsome-eats.js
+++ pawsome-eats.js
@@ -1,35 +1,64 @@
-// Fetch dog-friendly restaurants using the Yelp API with user input location
-function fetchDogFriendlyRestaurants() {
+// API endpoint URL with proxy server
+const apiUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
+
+// Yelp API credentials
+const apiKey = 'oRc9-C72u38kHp3pS2W7S42ezwi7n4JQIfA893O0DWRS6EMhkp8w2r6eYHJMjD87YLIiyeOPAV2dbm6qnfl-j41-tfGQMmQFpWpCdwZGWWCcRCNqA4KguDJPoeMpZHYx';
+
+// Search button click event
+document.getElementById('searchButton').addEventListener('click', function () {
     const locationInput = document.getElementById('locationInput').value;
-  
-    // Make an Axios request to the Yelp API
-    axios.get(`https://api.yelp.com/v3/businesses/search`, {
-      headers: {
-        'Authorization': 'Bearer oRc9-C72u38kHp3pS2W7S42ezwi7n4JQIfA893O0DWRS6EMhkp8w2r6eYHJMjD87YLIiyeOPAV2dbm6qnfl-j41-tfGQMmQFpWpCdwZGWWCcRCNqA4KguDJPoeMpZHYx' 
-      },
-      params: {
+    if (locationInput) {
+        // Clear previous results
+        document.getElementById('restaurantList').innerHTML = '';
+        // Call Yelp API
+        searchRestaurants(locationInput);
+    } else {
+        alert('Please enter a location to search.');
+    }
+});
+
+// Function to search restaurants using Yelp API
+function searchRestaurants(location) {
+    const headers = {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+    };
+
+    const params = {
         term: 'dog friendly',
-        location: locationInput
-      }
-    })
-      .then(response => {
-        const data = response.data;
-        const restaurantsList = document.getElementById('restaurants');
-        restaurantsList.innerHTML = '';
-        data.businesses.forEach(restaurant => {
-          // Render restaurant items as desired
-          const restaurantItem = document.createElement('li');
-          restaurantItem.textContent = restaurant.name;
-          restaurantsList.appendChild(restaurantItem);
+        location: location
+    };
+
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        headers: headers,
+        data: params,
+        success: function (response) {
+            displayRestaurants(response.businesses);
+        },
+        error: function (error) {
+            console.error('Error fetching data from Yelp API:', error);
+        }
+    });
+}
+
+// Function to display restaurant results
+function displayRestaurants(restaurants) {
+    const restaurantList = document.getElementById('restaurantList');
+
+    if (restaurants.length > 0) {
+        const ul = document.createElement('ul');
+        ul.className = 'restaurant-ul';
+        restaurantList.appendChild(ul);
+
+        restaurants.forEach(restaurant => {
+            const li = document.createElement('li');
+            li.className = 'restaurant-li';
+            li.textContent = restaurant.name;
+            ul.appendChild(li);
         });
-      })
-      .catch(error => {
-        console.error('Error fetching data from Yelp API:', error);
-      });
-  }
-  
-  // Attach event listener to search button
-  document.getElementById('searchButton').addEventListener('click', () => {
-    fetchDogFriendlyRestaurants();
-  });
-  
+    } else {
+        restaurantList.textContent = 'No restaurants found.';
+    }
+}
