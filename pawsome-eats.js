ed8e8d5e5333bf571b3ea9c6a81cbdc831932767
aditi-ashok <aditiash@usc.edu>
2023-04-15 12:41:21 -0700

    updated
    updated

--- pawsome-eats.js
+++ pawsome-eats.js
@@ -1,6 +1,7 @@
 // Fetch dog-friendly restaurants using Yelp API with user input location
 function fetchDogFriendlyRestaurants() {
     const locationInput = document.getElementById('locationInput').value;
+    console.log("location input: ", locationInput)
   
     const url = `https://api.yelp.com/v3/businesses/search?term=dog+friendly&location=${locationInput}`;
   
@@ -10,33 +11,33 @@ function fetchDogFriendlyRestaurants() {
         'Authorization': 'Bearer oRc9-C72u38kHp3pS2W7S42ezwi7n4JQIfA893O0DWRS6EMhkp8w2r6eYHJMjD87YLIiyeOPAV2dbm6qnfl-j41-tfGQMmQFpWpCdwZGWWCcRCNqA4KguDJPoeMpZHYx'
       }
     })
-    .then(response => {
-      const data = response.data;
-      const restaurantsList = document.getElementById('restaurants');
-      restaurantsList.innerHTML = '';
-      data.businesses.forEach(restaurant => {
-        const restaurantItem = document.createElement('li');
-        restaurantItem.classList.add('restaurant-item');
+      .then(response => {
+        const data = response.data;
+        const restaurantsList = document.getElementById('restaurants');
+        restaurantsList.innerHTML = '';
+        data.businesses.forEach(restaurant => {
+          const restaurantItem = document.createElement('li');
+          restaurantItem.classList.add('restaurant-item');
   
-        const restaurantName = document.createElement('h3');
-        restaurantName.textContent = restaurant.name;
+          const restaurantName = document.createElement('h3');
+          restaurantName.textContent = restaurant.name;
   
-        const restaurantAddress = document.createElement('p');
-        restaurantAddress.textContent = restaurant.location.address1;
+          const restaurantAddress = document.createElement('p');
+          restaurantAddress.textContent = restaurant.location.address1;
   
-        const restaurantPhone = document.createElement('p');
-        restaurantPhone.textContent = `Phone: ${restaurant.phone}`;
+          const restaurantPhone = document.createElement('p');
+          restaurantPhone.textContent = `Phone: ${restaurant.phone}`;
   
-        restaurantItem.appendChild(restaurantName);
-        restaurantItem.appendChild(restaurantAddress);
-        restaurantItem.appendChild(restaurantPhone);
+          restaurantItem.appendChild(restaurantName);
+          restaurantItem.appendChild(restaurantAddress);
+          restaurantItem.appendChild(restaurantPhone);
   
-        restaurantsList.appendChild(restaurantItem);
+          restaurantsList.appendChild(restaurantItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching data from Yelp API:', error);
       });
-    })
-    .catch(error => {
-      console.error('Error fetching data from Yelp API:', error);
-    });
   }
   
   // Attach event listener to search button
